8e86626f87697faeaf21546ab675c4d208671116
ryancgroup <47953402+ryancgroup@users.noreply.github.com>
2019-02-24 17:30:42 -0500

    update endpoint to point to AWS lambda

--- assets/js/scripts_v2.js
+++ assets/js/scripts_v2.js
@@ -1030,7 +1030,8 @@ $(document).ready(function() {
 			$('#contactform .btn-submit').html('Sending message ...');
 			$('#commentform .btn-submit').html('Sending comment ...');
 
-			var url = "/rcg/submitForm";
+			var url = "https://u6aoabqle8.execute-api.us-east-2.amazonaws.com/Stage/email";
+			//var url = "/rcg/submitForm";
 			//var url = "http://localhost:8080/rcg/submitForm"; 
 			if(document.getElementsByClassName('form-control error').length == 0){
 				$.ajax({
